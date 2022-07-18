bb535ec5f4493b98dd7cf99d5540fa3127203645
u <8usakura@gmail.com>
2022-07-18 13:20:25 +0900

    change

---
+++ .history/application/resources/views/index.blade_20220717114039.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inorganic_chemistry_application</title>
+    <link rel="stylesheet" href="css/reset.css" >
+    <link rel="stylesheet" href="css/index.css" />
+       
+</head>
+<body>
+    <div class=card>
+        <div class=memorize>  
+          <input type="checkbox" class=checkbox name="">
+          <div class=Chemical-formula>CaSO<span>4</span></div>
+          <div class=circle></div>
+        </div>   
+        <form> 
+          <input type="button" class=button name="" value="memo" onClick="setTF(this.form.text1,'visible')">
+          <input type="text" name="text1" style="visibility:hidden"><br>
+        </form> 
+        <br>
+        <br>
+          
+
+        <div class=memorize>  
+          <input type="checkbox" class=checkbox name="">
+          <div class=Chemical-formula>SrSO<span>4</span></div> 
+          <div class=circle></div>
+        </div>
+        <form> 
+          <input type="button" class=button name="" value="memo" onClick="setTF(this.form.text1,'visible')">
+          <input type="text" name="text1" style="visibility:hidden"><br>
+        </form> 
+        <br>
+        <br>
+
+
+        <div class=memorize>  
+          <input type="checkbox" class=checkbox name="">
+          <div class=Chemical-formula>BaSO<span>4</span></div> 
+          <div class=circle></div>
+        </div>
+        <form> 
+          <input type="button" class=button name="" value="memo" onClick="setTF(this.form.text1,'visible')">
+          <input type="text" name="text1" style="visibility:hidden"><br>
+        </form> 
+        <br>
+        <br>
+
+
+        <div class=memorize>  
+          <input type="checkbox" class=checkbox name="">
+          <div class=Chemical-formula>PbSO<span>4</span></div>
+          <div class=circle></div> 
+        </div>
+        <form> 
+          <input type="button" class=button name="" value="memo" onClick="setTF(this.form.text1,'visible')">
+          <input type="text" name="text1" style="visibility:hidden"><br>
+        </form> 
+        <br>
+        <br>
+
+        <div id="point">POINT
+           <p id="point-text"></p>
+        </div>
+
+        <button id="check">
+          <p id="check-text">CHECK</p>
+        </button>
+        
+        <ul class="page">
+          <li>Previous</li>
+          <li class="this">1</li>
+          <li><a href="/index2">2</a></li>
+          <li><a href="/index3">3</a></li>
+          <li><a href="/index2">Next</a></li>
+         </ul>    
+    </div>  
+    
+     <script language="JavaScript">
+      function setTF(fObj,vType)
+      {
+        fObj.style.visibility = vType;
+      };
+  
+      document.getElementById("point").onclick = function() {
+        document.getElementById("point-text").innerHTML = "硫酸の塩は本来,水に溶けやすいです。<br>アルカリ土類金属イオンやPb<span class=ion>2+</span>があると沈殿します。";
+        
+      };
+
+      document.getElementById("check").onclick = function() {
+        document.getElementById("check-text").innerHTML = "アルカリ土類金属　Ca Sr Ba Ra   周期表の2属に属します";
+        
+      }
+
+      document.getElementById("check").onclick = function() {
+        document.getElementById("check").idList.remove("check");
+      }
+
+
+      /* window.addEventListener('load', function(){
+        document.getElementById("check").addEventListener('click', function(){
+        alert("アルカリ土類金属　Ca Sr Ba Ra   周期表の2属に属します");
+       });
+      });*/
+       </script>   
+</body>
+</html>
---
+++ .history/application/resources/views/index4.blade_20220717101441.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inorganic_chemistry_application</title>
+    <link rel="stylesheet" href="css/reset.css" >
+    <link rel="stylesheet" href="css/index.css" />
+</head>
+<body>
+    <div class=card>
+ 
+        <div class=memorize>  
+          <input type="checkbox" class=checkbox name="">
+          <div class=Chemical-formula>Fe<span>2</span>O<span>3</span></div>
+          <div class=circle-reddish-brown></div> 
+        </div>
+        <form> 
+          <input type="button" class=button name="" value="memo" onClick="setTF(this.form.text1,'visible')">
+          <input type="text" name="text1" style="visibility:hidden"><br>
+        </form> 
+        <br>
+        <br>
+
+
+        <div class=memorize>  
+          <input type="checkbox" class=checkbox name="">
+          <div class=Chemical-formula>CuO</div> 
+          <div class=circle-black></div>  
+
+        </div>
+        <form> 
+          <input type="button" class=button name="" value="memo" onClick="setTF(this.form.text1,'visible')">
+          <input type="text" name="text1" style="visibility:hidden"><br>
+        </form> 
+        <br>
+        <br>
+
+        <div id="point">
+            <p id="point-text">POINT</p>
+        </div>
+ 
+        
+
+     <ul class="page">
+       <li><a href="/index3">Previous</a></li>
+       <li class="this">4</li>
+       <li><a href="/index5">5</li>
+       <li><a href="/index6">6</li>
+       <li><a href="/index5">Next</li>
+     </ul>
+       
+     
+
+
+    </div>
+    <script language="JavaScript">
+        function setTF(fObj,vType)
+        {
+          fObj.style.visibility = vType;
+        };
+    
+        document.getElementById("point").onclick = function() {
+        document.getElementById("point-text").innerHTML = "Fe<span class=kasuu>2</span>O<span class=kasuu>3</span>は赤褐色　CuOは赤褐色　の沈殿を作ります。";
+          
+        };
+    </script>  
+
+</body>
